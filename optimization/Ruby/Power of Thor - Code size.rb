df32c38a633cffa08d11a101ba068e436ec7f254
koukakaijin <koukakaijin@gmail.com>
2015-07-03 12:22:23 +0200

    Update  Power of Thor - Code size.rb (178)
    Code size: 178
    786/3231

--- optimization/Ruby/Power of Thor - Code size.rb
+++ optimization/Ruby/Power of Thor - Code size.rb
@@ -1,9 +1,1 @@
-z,w,a,b=gets.split.collect{|x|x.to_i}
-loop do
-x=z-a
-y=w-b
-y==0?j=x>0?"E":"W":x==0?j=y>0?"S":"N":x>0?j=y>0?"SE":"NE":j=y>0?"SW":"NW"
-x==0?x:x>0?a+=1:a-=1
-y==0?y:y>0?b+=1:b-=1
-puts j
-end
+z,w,a,b=gets.split.collect{|x|x.to_i};loop do;x=z-a;y=w-b;j=y==0?x>0?"E":"W":x==0?y>0?"S":"N":x>0?y>0?"SE":"NE":y>0?"SW":"NW";x==0?x:x>0?a+=1:a-=1;y==0?y:y>0?b+=1:b-=1;puts j;end
