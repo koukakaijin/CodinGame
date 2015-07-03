51ca1fcd7ff6c2c6761fdc960d2736fecda30ae7
koukakaijin <koukakaijin@gmail.com>
2015-07-03 13:34:53 +0200

    Update Power of Thor.rb
    Better code

--- easy/Ruby/Power of Thor.rb
+++ easy/Ruby/Power of Thor.rb
@@ -4,37 +4,11 @@ $LX, $LY, $initialTX, $initialTY = gets.split(" ").collect {|x| x.to_i}
 # game loop
 loop do
     $E = gets.to_i # The level of Thor's remaining energy, representing the number of moves he can still make.
-
     x = $LX - $initialTX
     y = $LY - $initialTY
-    
-    if (x>0 && y>0) 
-        puts "SE"
-        $initialTY=$initialTY+1
-        $initialTX=$initialTX+1
-    elsif (x>0 && y<0) 
-        puts "NE"
-        $initialTY=$initialTY-1
-        $initialTX=$initialTX+1
-    elsif (x>0 && y==0) 
-        puts "E"
-        $initialTX=$initialTX+1
-    elsif (x<0 && y>0)
-        puts "SW"
-        $initialTY=$initialTY+1
-        $initialTX=$initialTX-1
-    elsif (x<0 && y<0) 
-        puts "NW"
-        $initialTY=$initialTY-1
-        $initialTX=$initialTX-1
-    elsif (x<0 && y==0) 
-        puts "W"
-        $initialTX=$initialTX-1
-    elsif (x==0 && y>0) 
-        puts "S"
-        $initialTY=$initialTY+1
-    elsif (x==0 && y<0) 
-        puts "N"
-        $initialTY=$initialTY-1
-    end
+    j=y==0?"":y>0?"S":"N"
+    x==0?x:x>0?j+="E":j+="W"
+    x==0?x:x>0?$initialTX+=1:$initialTX-=1
+    y==0?y:y>0?$initialTY+=1:$initialTY-=1
+    puts j
 end
